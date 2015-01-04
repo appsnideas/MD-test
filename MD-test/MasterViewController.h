f261285985e602c4d8ebb2d13f50d5c114ee7bf2
Vamsi Nadella <vamsinadella@users.noreply.github.com>
2015-01-04 00:56:57 -0800

    Initial Commit

---
+++ MD-test/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  MD-test
+//
+//  Created by Vamsikrishna Nadella on 1/4/15.
+//  Copyright (c) 2015 appsnideas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
